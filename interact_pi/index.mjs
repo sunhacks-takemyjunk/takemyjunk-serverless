3731672e0006cb7e18a5318390601c72404e765e
jjin43 <jjin43@asu.edu>
2024-09-29 07:27:01 -0700

    pi CORS

--- interact_pi/index.mjs
+++ interact_pi/index.mjs
@@ -3,8 +3,26 @@ import { SNSClient, PublishCommand } from "@aws-sdk/client-sns";
 const snsClient = new SNSClient({ region: "us-east-1" });
 
 export const handler = async (event) => {
+    const headers = {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*', // Allow all origins
+        'Access-Control-Allow-Methods': 'GET, PUT, DELETE, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+    };
+
+    // Handle preflight OPTIONS request
+    if (event.httpMethod === 'OPTIONS') {
+        return {
+            statusCode: 200,
+            headers: headers,
+            body: JSON.stringify({ message: "CORS preflight response" })
+        };
+    }
+
+    const action = event.queryStringParameters.action;
+
     const params = {
-        Message: JSON.stringify({ action: "perform_hardware_action" }), // Customize the message as needed
+        Message: JSON.stringify({ action: action }), // 'open' or 'closed'
         TopicArn: "arn:aws:sns:us-east-1:122286528153:pi_tasks" // Replace with your SNS topic ARN
     };
 
@@ -12,12 +30,14 @@ export const handler = async (event) => {
         const data = await snsClient.send(new PublishCommand(params));
         return {
             statusCode: 200,
-            body: JSON.stringify({ message: "Message published to SNS", data: data })
+            headers: headers,
+            body: JSON.stringify({ message: "Action published to SNS", data: data })
         };
     } catch (error) {
         return {
             statusCode: 500,
-            body: JSON.stringify({ message: "Error publishing message to SNS", error: error.message })
+            headers: headers,
+            body: JSON.stringify({ message: "Error publishing action to SNS", error: error.message })
         };
     }
 };
